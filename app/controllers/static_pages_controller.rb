43ef241523b276877e32600dded17ecf3d34b002
peterbarnes <barnespeter213@gmail.com>
2014-07-28 13:00:51 -0500

    Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,21 @@
+class StaticPagesController < ApplicationController
+  def home
+    
+  end
+
+  def locations
+    
+  end
+
+  def services
+    
+  end
+
+  def contact
+    
+  end
+
+  def about
+    
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  match '/locations', to: 'static_pages#locations', via: 'get'
+  match '/services',  to: 'static_pages#services',  via: 'get'
+  match '/contact',   to: 'static_pages#contact',   via: 'get'
+  match '/about',     to: 'static_pages#about',     via: 'get'
+end
